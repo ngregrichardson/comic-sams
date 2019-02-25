72c95cad58aaedad45cc25189e3c722cae9425e6
Bert 10 <ngregrichardson@gmail.com>
2019-02-25 17:05:11 -0500

    Decreased loading times, added gif backup, slowed animations

--- changes.js
+++ changes.js
@@ -7,6 +7,8 @@ var h4 = document.getElementsByTagName('h4');
 var h5 = document.getElementsByTagName('h5');
 var h6 = document.getElementsByTagName('h6');
 var a = document.getElementsByTagName('a');
+var table = document.getElementsByTagName('table');
+var tr = document.getElementsByTagName('tr');
 var td = document.getElementsByTagName('td');
 var nobr = document.getElementsByTagName('nobr');
 var div = document.getElementsByTagName('div');
@@ -15,6 +17,15 @@ var span = document.getElementsByTagName('span');
 var img = document.getElementsByTagName('img');
 var button = document.getElementsByTagName('button');
 var input = document.getElementsByTagName('input');
+var blockquote = document.getElementsByTagName('blockquote');
+var canvas = document.getElementsByTagName('canvas');
+var footer = document.getElementsByTagName('footer');
+var header = document.getElementsByTagName('header');
+var select = document.getElementsByTagName('select');
+var option = document.getElementsByTagName('option');
+var center = document.getElementsByTagName('center');
+var fonts = [p, h1, h2, h3, h4, h5, h6, a, td, nobr, div, cite, span, button, input];
+var backgrounds = [p, h1, h2, h3, h4, h5, h6, a, table, tr, td, nobr, div, cite, span, img, button, input, blockquote, canvas, footer, header, select, option, center];
 
 /* Options Variables */
 var fontStatus;
@@ -27,12 +38,12 @@ var state;
 
 /* Gif Variables */
 var gifs = []; // Empty array for storing random gifs later
-var limit = 300; // How many gifs to get from giphy
+var limit = 100; // How many gifs to get from giphy
 var url; // Stores the URL to get the gif data from
 var response = 0; // Stores the response from the URL
 
 /* Options Getter */
-chrome.storage.sync.get(['fontStatus', 'fontColorStatus', 'spinStatus', 'gifStatus', 'backgroundStatus', 'searchTag', 'state'], function(status) {
+chrome.storage.sync.get(['fontStatus', 'fontColorStatus', 'spinStatus', 'gifStatus', 'backgroundStatus', 'searchTag', 'state'], function (status) {
   fontStatus = status.fontStatus;
   fontColorStatus = status.fontColorStatus;
   spinStatus = status.spinStatus;
@@ -44,59 +55,52 @@ chrome.storage.sync.get(['fontStatus', 'fontColorStatus', 'spinStatus', 'gifStat
 
 /* On Page Load */
 
-$(function() {
+$(function () {
   if (state) { // If enabled
 
     if (fontStatus) { // If fonts are enabled
       runFont(); // Start font intervals
-      setInterval(function() {
+      setInterval(function () {
         runFont();
       }, 100);
     }
 
-    if(fontColorStatus) { // If font colors are enabled
+    if (fontColorStatus) { // If font colors are enabled
+      fontColorSetup(); // Setup font colors
       runFontColor(); // Start font color intervals
-      setInterval(function() {
+      setInterval(function () {
         runFontColor();
       }, 100);
     }
 
-    if(spinStatus) {
-      runSpin(p);
-      runSpin(h1);
-      runSpin(h2);
-      runSpin(h3);
-      runSpin(h4);
-      runSpin(h5);
-      runSpin(h6);
-      runSpin(a);
-      runSpin(td);
-      runSpin(nobr);
-      runSpin(cite);
-      runSpin(span);
-      runSpin(button);
+    if (spinStatus) {
+      for (var i = 0; i < fonts.length - 6; i++) {
+        runSpin(fonts[i]);
+      }
     }
 
-    if(gifStatus) { // If gifs are enabled
+    if (gifStatus) { // If gifs are enabled
       url = 'https://api.giphy.com/v1/gifs/search?q=' + searchTag + '&api_key=NChfgHwB35TjflSoAYO3NY1O8u21pJKI&limit=' + limit; // Create the URL for gifs using the searchTag and limit
-      $.getJSON(url, function(data) { // Get the data from the url
+      $.getJSON(url, function (data) { // Get the data from the url
         response = data; // Make the data global
         // Make an array of all the gif URLs returned
         for (var i = 0; i < response.data.length; i++) {
           gifs[i] = "https://i.giphy.com/media/" + response.data[i].id + "/giphy.webp";
         }
 
-        runGif(); // Start gif intervals
-        setInterval(function() {
-          runGif();
-        }, 1700);
+        runGif(); // Set all gifs
+      }).fail(function (d) {
+        gifs[0] = 'https://nrdesign.xyz/comicsams/backupGif.webp';
+        runGif(); // Set all gifs
       });
     }
 
     if (backgroundStatus) { // If background is enabled
-      runBackground(div); // Run for all divs
-      runBackground(button); // Run for all buttons
-      setInterval(function() { // Start background intervals
+      backgroundColorSetup();
+      for (i = 0; i < backgrounds.length; i++) {
+        runBackground(backgrounds[i]);
+      }
+      setInterval(function () { // Start background intervals
         document.querySelector('body').style.background = randomColors();
       }, 100);
     }
@@ -108,10 +112,10 @@ $(function() {
 function runFont() {
   // Adds css for spinnning
   var css = '#spinning { animation-name: spinner; animation-timing-function: linear; animation-iteration-count: infinite; animation-duration: 5s; transform-style: preserve-3d; } @keyframes spinner { from { transform: rotateY(0deg); } to { transform: rotateY(-360deg); } }',
-  head = document.head || document.getElementsByTagName('head')[0],
-  style = document.createElement('style');
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
   style.type = 'text/css';
-  if (style.styleSheet){
+  if (style.styleSheet) {
     style.styleSheet.cssText = css;
   } else {
     style.appendChild(document.createTextNode(css));
@@ -119,46 +123,31 @@ function runFont() {
   head.appendChild(style);
 
   // Runs fonts for each HTML element
-  runText(p);
-  runText(h1);
-  runText(h2);
-  runText(h3);
-  runText(h4);
-  runText(h5);
-  runText(h6);
-  runText(a);
-  runText(td);
-  runText(nobr);
-  runText(div);
-  runText(cite);
-  runText(span);
-  runText(button);
-  runText(input);
+  for (var i = 0; i < fonts.length; i++) {
+    runText(fonts[i]);
+  }
 }
 
 /* Font Color Function*/
 
 function runFontColor() {
   // Runs font colors for each HTML element
-  runTextColor(p);
-  runTextColor(h1);
-  runTextColor(h2);
-  runTextColor(h3);
-  runTextColor(h4);
-  runTextColor(h5);
-  runTextColor(h6);
-  runTextColor(a);
-  runTextColor(td);
-  runTextColor(nobr);
-  runTextColor(div);
-  runTextColor(cite);
-  runTextColor(span);
-  runTextColor(button);
+  for (var i = 0; i < fonts.length; i++) {
+    runTextColor(fonts[i]);
+  }
 }
 
 function runText(text) {
   for (var i = 0, l = text.length; i < l; i++) { // For each text element
     text[i].style.fontFamily = "Comic Sans MS, cursive, sans-serif"; // Replace the font
+  }
+}
+
+function fontColorSetup() {
+  for (var i = 0, l = fonts.length; i < l; i++) {
+    for (var j = 0, m = fonts[i].length; j < m; j++) {
+      fonts[i][j].style.transition = 'color 0.2s ease';
+    }
   }
 }
 
@@ -171,20 +160,30 @@ function runTextColor(text) {
 function runSpin(text) {
   for (var i = 0, l = text.length; i < l; i++) { // For each text element
     text[i].style.display = "block"; // Make sure that it is block so it can be transformed
+    text[i].style.transformOrigin = '50% 0';
     text[i].setAttribute('id', 'spinning'); // Assign the spin ID to it
   }
 }
 
 function runGif() {
-    for (var i = 0, l = img.length; i < l; i++) { //  For each image
-      img[i].src = randomGif(); // Replace it with a random gif
+  for (var i = 0, l = img.length; i < l; i++) { //  For each image
+    img[i].srcset = '';
+    img[i].src = randomGif(); // Replace it with a random gif
+  }
+}
+
+function backgroundColorSetup() {
+  for (var i = 0, l = backgrounds.length; i < l; i++) {
+    for (var j = 0, m = backgrounds[i].length; j < m; j++) {
+      backgrounds[i][j].style.transition = 'background-color 0.2s ease';
     }
+  }
 }
 
 function runBackground(elem) {
-  setInterval(function() { // Start intervals
+  setInterval(function () { // Start intervals
     for (i = 0, l = elem.length; i < l; i++) { // For each element
-        elem[i].style.backgroundColor = randomColors(); // Give it a random background color
+      elem[i].style.backgroundColor = randomColors(); // Give it a random background color
     }
   }, 100);
 }
@@ -194,5 +193,7 @@ function randomColors() {
 }
 
 function randomGif() {
-  return gifs[Math.floor(Math.random() * limit - 1)]; // Find  a random gif
+  var g = Math.floor(Math.random() * gifs.length - 1);
+  if (g < 0) g = 0;
+  return gifs[g]; // Find  a random gif
 }
